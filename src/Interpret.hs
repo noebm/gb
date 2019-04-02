e4e2f4f8d960d04bf68769f276608f56247d3e90
noebm <moritz.noebauer@gmail.com>
2019-04-02 02:02:51 +0200

    More instructions

--- src/Interpret.hs
+++ src/Interpret.hs
@@ -3,7 +3,11 @@ module Interpret where
 import Data.Word
 import Data.Int
 import Data.Bits
+
 import Control.Lens
+import Control.Monad
+
+import Text.Printf
 
 import OpCode
 import Memory.Accessible
@@ -70,7 +74,10 @@ setArgM arg = case arg of
     store16 (Register16 HL) (hl - 1)
     store8 (Addr8 hl) b
 
-  x -> error $ "cannot write to " ++ show x
+  AddressFF -> Left (\b -> (`store8` b) . Addr8 . addrFF =<< byte)
+  Address   -> Left (\b -> (`store8` b) . Addr8 =<< word)
+
+  x -> error $ "setArgM: cannot write to " ++ show x
   -- ArgFlag f -> Left (load8 (Register8 F))
   -- Address    -> Right word
   -- AddressFF  -> Right (addrFF <$> byte)
@@ -87,13 +94,13 @@ interpretM instr@(Instruction b op args) = case op of
       sp <- load16 (Register16 SP)
       r <- sbyte
       store16 (Register16 HL) (addRelative sp r)
-    _ -> error "interpretM: LD - invalid arguments"
+    _ -> msg
   XOR -> case getArgM <$> args of
     [ Left g ] -> do
       v <- g
       a <- load8 (Register8 A)
       store8 (Register8 A) (a `xor` v)
-    _ -> error "interpretM: XOR - invalid arguments"
+    _ -> msg
   BIT -> case getArgM <$> args of
     [ Left g ] -> do
       let (_, y, _) = byteCodeDecompose b
@@ -102,5 +109,59 @@ interpretM instr@(Instruction b op args) = case op of
         & flagZ .~ not (v `testBit` fromIntegral y)
         & flagN .~ False
         & flagH .~ True
+    _ -> msg
+  JR -> case args of
+    [ arg ]
+      | Right g <- getArgM arg ->
+          g >>= store16 (Register16 PC)
+    [ ArgFlag f , arg ]
+      | Right g <- getArgM arg -> do
+          t <- getFlag f <$> load8 (Register8 F)
+          addr <- g
+          when t $ store16 (Register16 PC) addr
+    _ -> msg
+  JP -> case args of
+    [ arg ]
+      | Right g <- getArgM arg ->
+          g >>= store16 (Register16 PC)
+    [ ArgFlag f , arg ]
+      | Right g <- getArgM arg -> do
+          t <- getFlag f <$> load8 (Register8 F)
+          addr <- g
+          when t $ store16 (Register16 PC) addr
+    _ -> msg
+  CALL -> case args of
+    [ arg ]
+      | Right g <- getArgM arg -> do
+          push =<< load16 (Register16 PC)
+          g >>= store16 (Register16 PC)
+    [ ArgFlag f , arg ]
+      | Right g <- getArgM arg -> do
+          t <- getFlag f <$> load8 (Register8 F)
+          addr <- g
+          when t $ do
+            push =<< load16 (Register16 PC)
+            store16 (Register16 PC) addr
+    _ -> msg
+  RET -> case args of
+    [] -> pop >>= store16 (Register16 PC)
+    _ -> msg
+
+  INC -> case args of
+    [ arg ] | Right g <- getArgM arg
+            , Right s <- setArgM arg -> s . (+1) =<< g
+            | Left g <- getArgM arg
+            , Left s <- setArgM arg -> do
+                v <- g
+                let v' = v + 1
+                s v'
+                modifyFlags $ \f -> f
+                  & flagN .~ False
+                  & flagH .~ (v' `xor` v) `testBit` 3
+                  & flagC .~ (v' `xor` v) `testBit` 7
+    _ -> msg
+
   _ -> error $ "failed at " ++ show instr
+
+  where msg = error $ printf "interpretM: %s - invalid arguments" (show op)
 
