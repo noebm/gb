5525143595f345ce7134af3f575492c2205153f1
noebm <moritz.noebauer@gmail.com>
2019-04-02 20:25:57 +0200

    Update main

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,11 @@
 module Main where
 
+import System.Environment
 import Lib
 
 main :: IO ()
-main = someFunc
+main = do
+  args <- getArgs
+  case args of
+    [] -> someFunc Nothing
+    x : _ -> someFunc (Just x)
