84bb07145d5d4a4f485d45a60162b8a557eaad6b
noebm <moritz.noebauer@gmail.com>
2019-04-03 17:59:17 +0200

    Move interrupt clearing code for clarity

--- src/Interrupt.hs
+++ src/Interrupt.hs
@@ -17,11 +17,11 @@ newtype Interrupt = Interrupt Int
 enterInterrupt :: MonadEmulator m => Interrupt -> m Word
 enterInterrupt (Interrupt k) = do
   setIEM False
+  store8 interruptFlag . (`clearBit` k) =<< load8 interruptFlag
   -- wait 2 cycles
   push =<< load16 (Register16 PC) -- 2 cycles
   let addr = 0x0040 + 8 * fromIntegral k
   store16 (Register16 PC) addr    -- 1 cycle
-  store8 interruptFlag . (`clearBit` k) =<< load8 interruptFlag
   return 20 -- 4 * 5
 
 handleInterrupt :: MonadEmulator m => m (Maybe Interrupt)
