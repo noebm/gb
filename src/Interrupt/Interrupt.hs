60f424ba2dd7634fdf07381144481750fdca15f8
noebm <moritz.noebauer@gmail.com>
2020-01-05 04:14:05 +0100

    Simplify Interrupt implementation using lenses

--- src/Interrupt/Interrupt.hs
+++ src/Interrupt/Interrupt.hs
@@ -28,6 +28,7 @@ import Data.Foldable
 import Data.Word
 import Data.Bits
 import Control.Lens
+import Data.Bits.Lens
 
 data InterruptState = InterruptState
   { _interruptVBlank :: InterruptType
@@ -55,17 +56,31 @@ interruptAddress INTTIMER  = 0x50
 interruptAddress INTSERIAL = 0x58
 interruptAddress INTJOYPAD = 0x60
 
+{-# INLINE interrupt #-}
 interrupt :: Interrupt -> IndexedLens' Interrupt InterruptState InterruptType
-interrupt INTVBLANK p = interruptVBlank (indexed p INTVBLANK)
-interrupt INTLCD    p = interruptLCD    (indexed p INTLCD)
-interrupt INTTIMER  p = interruptTimer  (indexed p INTTIMER)
-interrupt INTSERIAL p = interruptSerial (indexed p INTSERIAL)
-interrupt INTJOYPAD p = interruptJoypad (indexed p INTJOYPAD)
+interrupt INTVBLANK f = interruptVBlank (indexed f INTVBLANK)
+interrupt INTLCD    f = interruptLCD    (indexed f INTLCD)
+interrupt INTTIMER  f = interruptTimer  (indexed f INTTIMER)
+interrupt INTSERIAL f = interruptSerial (indexed f INTSERIAL)
+interrupt INTJOYPAD f = interruptJoypad (indexed f INTJOYPAD)
+
+interrupts :: IndexedTraversal' Interrupt InterruptState InterruptType
+interrupts f s
+  = InterruptState
+    <$> aux INTVBLANK
+    <*> aux INTLCD
+    <*> aux INTTIMER
+    <*> aux INTSERIAL
+    <*> aux INTJOYPAD
+    <*> pure (s ^. interruptMasterEnableFlag)
+  where aux i = indexed f i (s ^. interrupt i)
 
 -- find first set interrupt ordered by priority
 checkForInterrupts :: InterruptState -> Maybe Interrupt
-checkForInterrupts is = find (\i -> is ^. interrupt i . to isTriggered) [ INTVBLANK ..]
+checkForInterrupts is = findIndexOf interrupts isTriggered is
 
+-- should be moved somewhere else
+-- probably inside processInterrupts
 handleInterrupt :: InterruptState -> Maybe (Interrupt, InterruptState)
 handleInterrupt s = do
   guard (s ^. interruptMasterEnableFlag)
@@ -81,32 +96,18 @@ interruptBit INTTIMER  = 2
 interruptBit INTSERIAL = 3
 interruptBit INTJOYPAD = 4
 
-setInterruptBit :: Interrupt -> (InterruptType -> Bool) -> InterruptState -> Word8
-setInterruptBit i f s = shift (fromIntegral $ fromEnum (f (s ^. interrupt i))) (interruptBit i)
-
-getInterruptState :: Word8 -> (InterruptType -> Bool) -> InterruptState -> Word8
-getInterruptState b f s = foldl (\acc i -> acc .|. g i) b [INTVBLANK ..]
-  where g i = setInterruptBit i f s
+loadInterrupt' l b  = ifoldrOf' (interrupts <. l) (\i x -> bitAt (interruptBit i) .~ x) b
+storeInterrupt' l b = interrupts <. l .@~ \i -> b ^. bitAt (interruptBit i)
 
 loadInterrupt :: InterruptState -> Word16 -> Word8
-loadInterrupt s 0xff0f = getInterruptState 0xe0 _interruptFlag    s
-loadInterrupt s 0xffff = getInterruptState 0x00 _interruptEnabled s
+loadInterrupt s 0xff0f = loadInterrupt' interruptFlag    0xe0 s
+loadInterrupt s 0xffff = loadInterrupt' interruptEnabled 0x00 s
 loadInterrupt _ _ = error "loadInterrupt: not an interrupt address"
 
-getInterruptBit :: Interrupt
-                -> (InterruptType -> Bool -> InterruptType)
-                -> (InterruptState -> Word8 -> InterruptState)
-getInterruptBit i f s b = s & interrupt i %~ \x -> f x (b `testBit` interruptBit i)
-
-updateInterruptState :: (InterruptType -> Bool -> InterruptType) -> InterruptState -> Word8 -> InterruptState
-updateInterruptState f s0 b = foldl
-  (\s i -> s & interrupt i %~ \x -> f x (b `testBit` interruptBit i))
-  s0 [INTVBLANK ..]
-
 storeInterrupt :: InterruptState -> Word16 -> Word8 -> InterruptState
-storeInterrupt s0 0xff0f = updateInterruptState (\it x -> set interruptFlag x it) s0
-storeInterrupt s0 0xffff = updateInterruptState (\it x -> set interruptEnabled x it) s0
-storeInterrupt _ _ = error "storeInterrupt: not an interrupt address"
+storeInterrupt s0 0xff0f b = storeInterrupt' interruptFlag b s0
+storeInterrupt s0 0xffff b = storeInterrupt' interruptEnabled b s0
+storeInterrupt _ _ _ = error "storeInterrupt: not an interrupt address"
 
 {-# INLINE inInterruptRange #-}
 inInterruptRange :: Word16 -> Bool
