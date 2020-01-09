bd8f23fa37d84614237deca8359439d62d7af2ca
noebm <moritz.noebauer@gmail.com>
2020-01-09 15:19:47 +0100

    Add OAM interface and some more accessors to Sprite

--- src/GPU/Sprite.hs
+++ src/GPU/Sprite.hs
@@ -1,17 +1,38 @@
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
-module GPU.Sprite where
+module GPU.Sprite
+  ( Sprite
+  -- | Accessors for Sprite
+  , spritePositionX, spritePositionY, spritePosition
+  , spriteFlippedX, spriteFlippedY, spriteFlipped
+  , spriteBGPriority
+  , spriteDMGPalette
+
+  -- | Object Access Memory
+  , OAM
+  , defaultOAM, dumpOAM
+  , loadOAM, storeOAM
+  , getLineSprites
+  , directMemoryAccessOAM
+  )
+where
 
 import qualified Data.Vector.Unboxed.Base as U
 import qualified Data.Vector.Generic as G
 import qualified Data.Vector.Generic.Mutable as M
 
 import Control.Monad
+import Control.Applicative
 
 import Control.Lens
 import Data.Bits.Lens
 import Data.Word
+import SDL.Vect
+
+import Data.Bits
+
+import GPU.GPUControl
 
 data Sprite = Sprite
   { _spritePositionY  :: {-# UNPACK #-} !Word8
@@ -19,6 +40,8 @@ data Sprite = Sprite
   , _spriteTile       :: {-# UNPACK #-} !Word8
   , _spriteAttributes :: {-# UNPACK #-} !Word8
   }
+
+defaultSprite = Sprite 0x00 0x00 0x00 0x00
 
 data instance U.Vector    Sprite =  V_Sprite {-# UNPACK #-} !Int !(U.Vector Word8)
 data instance U.MVector s Sprite = MV_Sprite {-# UNPACK #-} !Int !(U.MVector s Word8)
@@ -60,16 +83,59 @@ instance G.Vector U.Vector Sprite where
 
 makeLenses ''Sprite
 
+spritePosition :: Lens' Sprite (V2 Word8)
+spritePosition f (Sprite y x t attr) = (\(V2 x' y') -> Sprite y' x' t attr) <$> f (V2 x y)
+
 spriteFlippedX :: Lens' Sprite Bool
 spriteFlippedX = spriteAttributes . bitAt 5
 
 spriteFlippedY :: Lens' Sprite Bool
 spriteFlippedY = spriteAttributes . bitAt 6
 
+spriteFlipped :: Lens' Sprite (V2 Bool)
+spriteFlipped f sprite
+  = fmap (foldr ($) sprite . liftA2 set (V2 spriteFlippedX spriteFlippedY))
+  $ f $ liftA2 view (V2 spriteFlippedX spriteFlippedY) (pure sprite)
+
 spriteBGPriority :: Lens' Sprite Bool
 spriteBGPriority = spriteAttributes . bitAt 7
 
+-- XXX GBC palette (at bit 0 + 1) missing
 spriteDMGPalette :: Lens' Sprite Bool
 spriteDMGPalette = spriteAttributes . bitAt 4
 
+spriteBytes :: Traversal' Sprite Word8
+spriteBytes f (Sprite a b c d) = Sprite <$> f a <*> f b <*> f c <*> f d
 
+newtype OAM = OAM (U.Vector Sprite)
+
+defaultOAM :: OAM
+defaultOAM = OAM (G.replicate 40 defaultSprite)
+
+dumpOAM :: OAM -> [ Word8 ]
+dumpOAM = G.toList . view oamBytesInternal
+
+{-# INLINE oamBytesInternal #-}
+oamBytesInternal :: Lens' OAM (U.Vector Word8)
+oamBytesInternal f (OAM (V_Sprite n v)) = OAM . V_Sprite n <$> f v
+
+loadOAM :: GPUControl -> OAM -> Word16 -> Maybe Word8
+loadOAM GPUControl { _gpuMode = mode } oam addr = do
+  guard (mode /= ModeVRAM || mode /= ModeOAM)
+  G.indexM (oam ^. oamBytesInternal) $ fromIntegral (addr .&. 0x9f)
+
+storeOAM :: GPUControl -> Word16 -> Word8 -> OAM -> Maybe OAM
+storeOAM GPUControl { _gpuMode = mode } addr b oam = do
+  guard (mode /= ModeVRAM || mode /= ModeOAM)
+  return $ oam & oamBytesInternal.ix (fromIntegral (addr .&. 0x9f)) .~ b
+
+getLineSprites :: Word8 -> Word8 -> OAM -> U.Vector Sprite
+getLineSprites line size (OAM oam)
+  = G.take 10
+  $ G.filter (\s -> line - s ^. spritePositionY < size)
+  $ oam
+
+directMemoryAccessOAM :: U.Vector Word8 -> OAM
+directMemoryAccessOAM v
+  | G.length v == 0xa0 = OAM (V_Sprite 40 v)
+  | otherwise          = error $ "directMemoryAccessOAM: incorrect length - expected " ++ show 0xa0 ++ " but got " ++ show (G.length v)
