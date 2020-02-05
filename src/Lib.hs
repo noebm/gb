4d4a85007e142dc07df3cdb36a7b00a9eae62ee1
noebm <moritz.noebauer@gmail.com>
2020-02-05 16:56:54 +0100

    Remove all unused import and obsolete segments in Lib

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,12 +1,4 @@
-{-# LANGUAGE RankNTypes #-}
-module Lib
-where
-
-import Data.Foldable
-import Data.Word
-import Data.Bits
-
-import Text.Printf
+module Lib where
 
 import Control.Monad.IO.Class
 import Control.Monad
@@ -17,67 +9,18 @@ import GB
 
 import Instruction.Interpret
 import Instruction.Instruction
-import Interrupt.Interrupt
-import Interrupt.InterruptType
 
 import Cartridge.Cartridge
 import Cartridge.BootRom
-
 import GPU.GPUState
-import GPU.Drawing
-
 import Timer
-import Joypad (Joypad(..))
-import SDL.Input.Keyboard
-import SDL.Input.Keyboard.Codes
-
-import qualified SDL
-import Data.Maybe
-import Control.Applicative
-import Control.Lens
-import Data.Traversable
-
-import Utilities.SDL
-import qualified Data.Set as Set
-
-  {-
-joypadMapping :: Joypad -> Scancode
-joypadMapping JoypadUp    = ScancodeUp
-joypadMapping JoypadDown  = ScancodeDown
-joypadMapping JoypadLeft  = ScancodeLeft
-joypadMapping JoypadRight = ScancodeRight
-
-joypadMapping JoypadA = ScancodeZ
-joypadMapping JoypadB = ScancodeX
-joypadMapping JoypadStart  = ScancodeReturn
-joypadMapping JoypadSelect = ScancodeSpace
-
-keyboardEvents :: MonadIO m => Set.Set Scancode -> m (Set.Set Scancode)
-keyboardEvents s = do
-  evs <- SDL.pollEvents <&> toListOf (traverse . eventPayload . _KeyboardEvent)
-  let aux :: SDL.KeyboardEventData -> Set.Set Scancode -> Set.Set Scancode
-      aux k = case k ^. keyMotion of
-        SDL.Pressed  -> Set.insert sc
-        SDL.Released -> Set.delete sc
-        where sc = k ^. keyKeysym.keyScancode
-  return $ foldrOf folded aux s evs
-
-updateJoypad s = do
-  s' <- keyboardEvents s
-  let f x = x `Set.member` s'
-  -- liftIO . print =<< getJoypad
-  changed <- updateJoypadGB $ f . joypadMapping
-  when changed $
-    modifyInterrupt $ interruptJoypad . interruptFlag .~ True
-  return s'
--}
 
 updateCPU :: MonadEmulator m => m (Maybe Instruction, Word)
 updateCPU = do
   halted <- halt
   if not halted then do
     ime <- getIEM
-    when ime $ void $ processInterrupts
+    when ime $ void processInterrupts
     i <- parseInstructionM byte
     dt <- interpretM i
     return (Just i , dt)
@@ -112,8 +55,7 @@ mainloop fp' = do
       -- logger = Just $ \addr i -> do
       --   when (addr > 0xFF) $ putStrLn $ printf "0x%04x: %s" addr (show i)
 
-    let update fx s = do
-          -- forM_ [0..0] $ \_ -> do
+    let update fx = do
           pc <- loadPC
           t <- getCycles
           (i, dt') <- updateCPU
@@ -124,12 +66,11 @@ mainloop fp' = do
           updateGraphics fx dt
           updateTimer dt
           -- updateJoypad s
-          return s
 
-    let runTillStop fx s0 = do
-          s1 <- update fx s0
+    let runTillStop fx = do
+          update fx
           s <- stop
-          unless s $ runTillStop fx s1
+          unless s $ runTillStop fx
 
     gfx <- initializeGraphics
-    runTillStop gfx Set.empty
+    runTillStop gfx
