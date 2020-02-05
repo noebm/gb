0d6ff729c42f255b1c2085dba004fa1149fbc371
noebm <moritz.noebauer@gmail.com>
2020-02-05 16:02:50 +0100

    Basic commandline options setup

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,31 @@
 module Main where
 
 import System.Environment
+import System.Exit
+import System.IO
+import System.Console.GetOpt
 import Lib
+
+data Options = Help
+  deriving Eq
+
+options =
+  [ Option ['h'] ["help"] (NoArg Help) "Print this help message"
+  ]
+
+parseCommandline argv = case getOpt Permute options argv of
+  (opts, files, [])
+    | Help `elem` opts -> do
+        hPutStrLn stderr (usageInfo header options)
+        exitWith ExitSuccess
+    | [ file ] <- files -> return file
+  (_,_, errs) -> do
+    hPutStrLn stderr (concat errs ++ usageInfo header options)
+    exitWith (ExitFailure 1)
+  where
+    header = "Usage: <exec> [OPTION..] rom"
 
 main :: IO ()
 main = do
-  args <- getArgs
-  case args of
-    [] -> someFunc Nothing
-    x : _ -> someFunc (Just x)
+  file <- parseCommandline =<< getArgs
+  someFunc (Just file)
