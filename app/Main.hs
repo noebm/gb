afc9b1262b5ad28acc5d18560703c5d6ec6deaf8
noebm <moritz.noebauer@gmail.com>
2020-02-09 02:57:08 +0100

    Add option to view rom header

--- app/Main.hs
+++ app/Main.hs
@@ -4,19 +4,27 @@ import System.Environment
 import System.Exit
 import System.IO
 import System.Console.GetOpt
-import Lib
 
-data Options = Help
+import Lib
+import Cartridge.Cartridge
+
+data Options = Help | Info
   deriving Eq
 
 options =
   [ Option ['h'] ["help"] (NoArg Help) "Print this help message"
+  , Option ['i'] ["info"] (NoArg Info) "Display rom information"
   ]
 
 parseCommandline argv = case getOpt Permute options argv of
   (opts, files, [])
     | Help `elem` opts -> do
         hPutStrLn stderr (usageInfo header options)
+        exitWith ExitSuccess
+    | Info `elem` opts
+    , [ file ] <- files -> do
+        romOrError <- readRom file
+        flip (either putStrLn) romOrError $ \(Rom h _) -> print h
         exitWith ExitSuccess
     | [ file ] <- files -> return file
   (_,_, errs) -> do
